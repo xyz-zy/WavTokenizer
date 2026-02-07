0f77642a911190f263ea000cbe05a456b875216d
Lucy Zhao <lxz@mit.edu>
2026-02-07 18:22:21 -0500

    Distributed kmeans; threshold*world_size

--- encoder/quantization/core_vq.py
+++ encoder/quantization/core_vq.py
@@ -184,6 +184,78 @@ def kmeans(
     return means, bins
 
 
+def batched_bincount(x, *, minlength):
+    batch, dtype, device = x.shape[0], x.dtype, x.device
+    target = torch.zeros(batch, minlength, dtype = dtype, device = device)
+    values = torch.ones_like(x)
+    target.scatter_add_(-1, x, values)
+    return target
+
+
+
+def noop(*args, **kwargs):
+    pass
+
+def l2norm(t, dim = -1,  eps = 1e-6):
+    return F.normalize(t, p = 2, dim = dim, eps = eps)
+
+def batched_sample_vectors(samples, num):
+    return torch.stack([sample_vectors(sample, num) for sample in samples.unbind(dim = 0)], dim = 0)
+
+from einops import reduce
+
+def cdist(x, y, eps = 1e-8):
+    x2 = reduce(x ** 2, 'b n d -> b n', 'sum')
+    y2 = reduce(y ** 2, 'b n d -> b n', 'sum')
+    xy = torch.einsum('b i d, b j d -> b i j', x, y) * -2
+    return (rearrange(x2, 'b i -> b i 1') + rearrange(y2, 'b j -> b 1 j') + xy).clamp(min = eps).sqrt()
+
+
+def kmeans_distrib(
+    samples,
+    num_clusters,
+    num_iters = 10,
+    use_cosine_sim = False,
+    sample_fn = batched_sample_vectors,
+    all_reduce_fn = noop,
+    return_history = False,
+):
+    num_codebooks, dim, dtype, device = samples.shape[0], samples.shape[-1], samples.dtype, samples.device
+
+    means = sample_fn(samples, num_clusters)
+
+    for _ in range(num_iters):
+        if use_cosine_sim:
+            dists = samples @ rearrange(means, 'h n d -> h d n')
+        else:
+            dists = -cdist(samples, means)
+
+        buckets = torch.argmax(dists, dim = -1)
+        bins = batched_bincount(buckets, minlength = num_clusters)
+        all_reduce_fn(bins)
+
+        zero_mask = bins == 0
+        bins_min_clamped = bins.masked_fill(zero_mask, 1)
+
+        new_means = buckets.new_zeros(num_codebooks, num_clusters, dim, dtype = dtype)
+
+        new_means.scatter_add_(1, repeat(buckets, 'h n -> h n d', d = dim), samples)
+        new_means = new_means / rearrange(bins_min_clamped, '... -> ... 1')
+        all_reduce_fn(new_means)
+
+        if use_cosine_sim:
+            new_means = l2norm(new_means)
+
+        means = torch.where(
+            rearrange(zero_mask, '... -> ... 1'),
+            means,
+            new_means
+        )
+    if return_history:
+        # TODO: compute inertia in distributed setting
+        return means, bins, None
+    return means, bins
+
 class EuclideanCodebook(nn.Module):
     """Codebook with Euclidean distance.
     Args:
@@ -234,10 +306,20 @@ class EuclideanCodebook(nn.Module):
         if self.inited:
             return
 
-        embed, cluster_size, kmeans_history = kmeans(
-            data, self.codebook_size, self.kmeans_iters, return_history=True
-        ) #data不变
+        if distrib.is_distributed():
+            data = rearrange(data, 'n d -> 1 n d')
+            embed, cluster_size, kmeans_history = kmeans_distrib(
+                data, self.codebook_size, self.kmeans_iters,
+                sample_fn=sample_vectors_distributed,
+                all_reduce_fn=distrib.all_reduce,
+                return_history=True,
+            )
+            embed = embed[0]
+            cluster_size = cluster_size[0]
+        else:
+            embed, cluster_size, kmeans_history = kmeans(data, self.codebook_size, self.kmeans_iters, return_history=True) #data不变
         self.kmeans_history = kmeans_history
+        self.embed.data.copy_(embed)
         self.embed_avg.data.copy_(embed.clone())
         self.cluster_size.data.copy_(cluster_size)
         self.inited.data.copy_(torch.Tensor([True]))
@@ -267,7 +349,7 @@ class EuclideanCodebook(nn.Module):
         if self.threshold_ema_dead_code == 0:
             return
 
-        expired_codes = self.cluster_size < self.threshold_ema_dead_code
+        expired_codes = self.cluster_size < self.threshold_ema_dead_code * distrib.world_size()
         if not torch.any(expired_codes):
             return
         self.expired_codes = expired_codes.sum().item()
